33948d3829cc3be3abd05b58eec0eccdd3519b12
Liam McDermott <liam@woolwichweb.works>
2025-09-08 23:09:39 -0400

    Add fake top-level options to minimal environment module.

--- src/modules/top-level/minimal-env.nix
+++ src/modules/top-level/minimal-env.nix
@@ -7,6 +7,18 @@
     description = "Hostenv configuration for the current environment.";
   };
 
+  # # Fake options
+  # Allow merging user hostenv.nix modules, while ignoring the bits we
+  # have no interest in.
+  options.systemd = lib.mkOption {
+    type = lib.types.attr;
+    description = "In a minimal hostenv environment this option is ignored.";
+  };
+  options.services = lib.mkOption {
+    type = lib.types.attr;
+    description = "In a minimal hostenv environment this option is ignored.";
+  };
+
   imports = [
     ../environments.nix
     ../hostenv.nix
