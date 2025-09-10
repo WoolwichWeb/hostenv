f274e6a640ca4504103f65a008d472b0735806a8
Liam McDermott <liam@woolwichweb.works>
2025-09-10 14:25:51 -0400

    Fix virtualhost redirect to only occur if not overridden by hostenv.nix.

--- src/modules/environments.nix
+++ src/modules/environments.nix
@@ -2,6 +2,11 @@
 { config, lib, ... }:
 let
   cfg = config.environments;
+  defaultPriority = 1000;
+
+  # Flag for when the user has set the value of an option to
+  # `lib.mkForce null` and post-processing should respect that.
+  forceNull = "__HOSTENV_INTERNAL_DO_NOT_CHANGE_SEMAPHORE__";
 
   # `config.hostenv` is not mandatory, as this module may be evaluated as
   # part of a full hostenv build, or separately.
@@ -28,7 +33,7 @@ let
 
   };
 
-  environment = with lib.types; { config, name, allUsers, ... }: {
+  environment = with lib.types; { config, name, allUsers, options, ... }: {
     options = {
       enable = lib.mkEnableOption "this environment on hostenv";
 
@@ -48,92 +53,116 @@ let
         '';
       };
 
-      virtualHosts = lib.mkOption {
-        type = attrsOf (submodule {
-          options = {
+      virtualHosts =
+        let envConfig = config;
+        in lib.mkOption {
+          type = attrsOf
+            (submodule ({ options, config, ... }: {
+              options = {
 
-            locations = lib.mkOption {
-              type = attrsOf (submodule {
-                options = {
-                  return = lib.mkOption {
-                    type =
-                      with types;
-                      nullOr (oneOf [
-                        str
-                        int
-                      ]);
-                    default = null;
-                    example = "301 http://example.com$request_uri";
-                    description = ''
-                      Adds a return directive, for e.g. redirections.
-                    '';
-                  };
+                locations = lib.mkOption {
+                  type = attrsOf (submodule {
+                    options = {
+                      return = lib.mkOption {
+                        type =
+                          with types;
+                          nullOr (oneOf [
+                            str
+                            int
+                          ]);
+                        default = null;
+                        example = "301 http://example.com$request_uri";
+                        description = ''
+                          Adds a return directive, for e.g. redirections.
+                        '';
+                      };
+                    };
+                  });
+                  default = { };
                 };
-              });
-              default = { };
-            };
 
-            enableLetsEncrypt = lib.mkOption {
-              default = true;
-              type = bool;
-              description = ''
-                Enable Let's Encrypt for this host.
-              '';
-            };
+                enableLetsEncrypt = lib.mkOption {
+                  default = true;
+                  type = bool;
+                  description = ''
+                    Enable Let's Encrypt for this host.
+                  '';
+                };
 
-            globalRedirect = lib.mkOption {
-              type = nullOr str;
-              default = null;
-              example = "newserver.example.org";
-              description = ''
-                If set, all requests for this host are redirected to the given
-                hostname (the HTTP status code defaults to 301, configurable
-                with `redirectCode`).
-              '';
-            };
+                globalRedirect = lib.mkOption {
+                  type = nullOr str;
+                  default = null;
+                  example = "newserver.example.org";
+                  description = ''
+                    If set, all requests for this host are redirected to the given
+                    hostname (the HTTP status code defaults to 301, configurable
+                    with `redirectCode`).
+                  '';
+                  apply =
+                    let
+                      hostName = envConfig.hostenv.hostname;
+                      thisHost = config._module.args.name;
+                      thisPrio = options.globalRedirect.highestPrio;
+                    in
+                    # The user has set the redirect to `null` and wants us to
+                      # respect that.
+                    v:
+                    if thisHost == hostName && thisPrio < defaultPriority && v == null
+                    then forceNull
+                    else v;
+                };
 
-            redirectCode = lib.mkOption {
-              type = ints.between 300 399;
-              default = 301;
-              example = 308;
-              description = ''
-                HTTP status used by `globalRedirect` and `forceSSL`. Possible usecases
-                include temporary (302, 307) redirects, keeping the request method and
-                body (307, 308), or explicitly resetting the method to GET (303).
-                See <https://developer.mozilla.org/en-US/docs/Web/HTTP/Redirections>.
-              '';
-            };
+                redirectCode = lib.mkOption {
+                  type = ints.between 300 399;
+                  default = 301;
+                  example = 308;
+                  description = ''
+                    HTTP status used by `globalRedirect` and `forceSSL`. Possible usecases
+                    include temporary (302, 307) redirects, keeping the request method and
+                    body (307, 308), or explicitly resetting the method to GET (303).
+                    See <https://developer.mozilla.org/en-US/docs/Web/HTTP/Redirections>.
+                  '';
+                };
 
-            basicAuthFile = lib.mkOption {
-              type = nullOr path;
-              default = null;
-              description = ''
-                Basic Auth password file for a vhost.
-                Can be created by running {command}`nix-shell --packages apacheHttpd --run 'htpasswd -B -c FILENAME USERNAME'`.
-              '';
-            };
+                basicAuthFile = lib.mkOption {
+                  type = nullOr path;
+                  default = null;
+                  description = ''
+                    Basic Auth password file for a vhost.
+                    Can be created by running {command}`nix-shell --packages apacheHttpd --run 'htpasswd -B -c FILENAME USERNAME'`.
+                  '';
+                };
 
-          };
-        });
-        default = { };
-        apply = v:
-          let
-            hostname = config.hostenv.hostname;
-            # Virtual hosts that are valid redirection targets for the hostenv
-            # assigned *.hostenv.sh hostname.
-            # Note about Let's Encrypt: the `enableLetsEncrypt` option may be
-            # disabled if the user is still pointing the domain, so we do not
-            # consider the hostname a valid target in that case. This will
-            # probably have to be revisited later, especially once support for
-            # custom certificates is added.
-            targetVHosts = lib.filterAttrs (n: val: n != hostname && val.globalRedirect == null && val.enableLetsEncrypt == true) v;
-          in
-          # Tests if there is a valid target to redirect to, and that the user
-            # hasn't already set a redirect target.
-          if builtins.length (builtins.attrValues targetVHosts) > 0 && v.${hostname}.globalRedirect == null
-          then v // { ${hostname} = v.${hostname} // { globalRedirect = (builtins.head (lib.attrsToList targetVHosts)).name; }; }
-          else v;
-      };
+              };
+            }));
+          default = { };
+          apply = v:
+            let
+              hostname = config.hostenv.hostname;
+              # Virtual hosts that are valid redirection targets for the hostenv
+              # assigned *.hostenv.sh hostname.
+              # Note about Let's Encrypt: the `enableLetsEncrypt` option may be
+              # disabled if the user is still pointing the domain, so we do not
+              # consider the hostname a valid target in that case. This will
+              # probably have to be revisited later, especially once support for
+              # custom certificates is added.
+              targetVHosts = lib.filterAttrs
+                (n: val: n != hostname && val.globalRedirect == null && val.enableLetsEncrypt == true)
+                v;
+
+              setRedirect = v: target:
+                v // {
+                  ${hostname} = v.${hostname} // { globalRedirect = target; };
+                };
+            in
+            # Tests if there is a valid target to redirect to, and that the user
+              # hasn't already set a redirect target.
+            if v.${hostname}.globalRedirect == null && builtins.length (builtins.attrValues targetVHosts) > 0
+            then setRedirect v (builtins.head (lib.attrsToList targetVHosts)).name
+            else if v.${hostname}.globalRedirect == forceNull
+            then setRedirect v null
+            else v;
+        };
 
       hostenv = lib.mkOption {
         type = with lib.types; submoduleWith {
