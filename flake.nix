ee5f5b006051cb92484afeec0d58e801cc48a987
Liam McDermott <liam@woolwichweb.works>
2025-10-07 16:00:24 -0400

    Add app for looking up option documentation.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,81 @@
 {
-  description = "Hostenv - Your PaaS";
-  outputs =
-    { ... }:
+  description = "Hostenv: the PaaS you control";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    search = {
+      url = "github:NuschtOS/search";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, search, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = (import nixpkgs) {
+            inherit system;
+          };
+
+          docSearch = search.packages.${system}.mkMultiSearch {
+            title = "hostenv options search";
+            scopes = [
+              {
+                modules = [
+                  ./src/modules/top-level/full-env.nix
+                  { _module.args = { inherit pkgs; }; }
+                  ({ config, ... }: {
+                    hostenv = {
+                      organisation = "org";
+                      project = "example";
+                      root = ./.;
+                      environmentName = "main";
+                    };
+                  })
+                  {
+                    environments.main = {
+                      enable = true;
+                      type = "production";
+                      virtualHosts = {
+                        "www.example.com" = { };
+                      };
+                    };
+                  }
+                ];
+                urlPrefix = "https://gitlab.com/woolwichweb/hostenv/-/blob/main/";
+              }
+            ];
+          };
+
+          serveDocs = pkgs.writeShellApplication {
+            name = "serve-docs";
+            runtimeInputs = [ pkgs.http-server ];
+            runtimeEnv.server_flags = [
+              # Search for available port
+              "--port=0"
+
+              # Disable browser cache
+              "-c-1"
+
+              # Open using xdg-open
+              "-o"
+            ];
+            text = ''
+              http-server ${docSearch} "''${server_flags[@]}"
+            '';
+          };
+        in
+        {
+          apps.default = flake-utils.lib.mkApp { drv = serveDocs; };
+          packages = {
+            # Searchable documentation package.
+            inherit docSearch;
+          };
+        }) //
     {
       templates = {
         default = {
