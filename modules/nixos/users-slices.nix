bc60930fd25cb3bc0283ef4669e6c7e105d12183
Liam McDermott <liam@woolwichweb.works>
2025-12-12 21:34:34 -0500

    Fix slice definitions being overwritten in users-slices module

--- modules/nixos/users-slices.nix
+++ modules/nixos/users-slices.nix
@@ -19,28 +19,48 @@ in
 
   config = lib.mkIf (envs != { }) {
     users = {
-      users = lib.mapAttrs (name: env: {
-        isNormalUser = true;
-        uid = lib.mkDefault null;
-        group = name;
-        createHome = true;
-        openssh.authorizedKeys.keys = env.extras.publicKeys or [ ];
-        linger = true;
-      }) envs;
+      users = lib.mapAttrs
+        (name: env: {
+          isNormalUser = true;
+          uid = lib.mkDefault null;
+          group = name;
+          createHome = true;
+          openssh.authorizedKeys.keys = env.extras.publicKeys or [ ];
+          linger = true;
+        })
+        envs;
 
       groups = lib.mapAttrs (_: env: { }) envs;
     };
 
+    # For information about slice hierarchy, see:
+    # https://systemd.io/CONTROL_GROUP_INTERFACE/
+    #
+    # Quote:
+    # > Slices may be used to group a number of services and scopes together
+    # > in a hierarchial tree. Slices do not contain processes themselves,
+    # > but the services and scopes contained in them do. Slices are named in
+    # > the style of foobar-waldo.slice, where the path to the location of the
+    # > slice in the tree is encoded in the name with “-“ as separator for
+    # > the path components (foobar-waldo.slice is hence a subslice of
+    # > foobar.slice). There’s one special slices defined, -.slice, which
+    # > is the root slice of all slices (foobar.slice is hence subslice of
+    # > -.slice). This is similar how in regular file paths, “/” denotes the
+    # > root directory.
     systemd = {
-      slices = lib.mapAttrs (name: env: {
-        description = "${name} slice";
-        sliceConfig = config.hostenv.sliceDefaults;
-      }) envs // lib.mapAttrs (name: _env: { }) envs;
+      slices = lib.mapAttrs
+        (name: env: {
+          description = "${name} slice";
+          sliceConfig = config.hostenv.sliceDefaults;
+        })
+        envs;
 
-      services = lib.mapAttrs (name: env: {
-        overrideStrategy = "asDropin";
-        serviceConfig.Slice = "${name}-${toString (env.extras.uid or 0)}.slice";
-      }) envs;
+      services = lib.mapAttrs
+        (name: env: {
+          overrideStrategy = "asDropin";
+          serviceConfig.Slice = "${name}-${toString (env.extras.uid or 0)}.slice";
+        })
+        envs;
     };
   };
 }
