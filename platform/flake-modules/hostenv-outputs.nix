b9513a75cc61fad2ac57696b65f58e1d0473c6dc
Liam McDermott <liam@woolwichweb.works>
2025-12-16 18:07:50 -0500

    Fix to add option for eval.

--- platform/flake-modules/hostenv-outputs.nix
+++ platform/flake-modules/hostenv-outputs.nix
@@ -10,21 +10,34 @@ in
         type = types.functionTo (types.functionTo types.unspecified);
         default = inputs.self.makeHostenv.${system};
       };
+
       modules = lib.mkOption {
         type = types.listOf types.deferredModule;
         default = [ ];
       };
+
       environmentName = lib.mkOption {
         type = types.nullOr types.str;
         default = null;
       };
+
+      eval = lib.mkOption {
+        type = types.unspecified;
+        readOnly = true;
+        internal = true;
+        description = "Internal: full evalModules result for this system.";
+      };
+
+      outputs = lib.mkOption {
+        type = types.unspecified;
+        readOnly = true;
+        description = "Export-ready hostenv discovery outputs.";
+      };
     };
 
     config.hostenvProject.eval =
-      let
-        mk = config.hostenvProject.makeHostenv;
-      in
-      mk config.hostenvProject.modules config.hostenvProject.environmentName;
+      let mk = config.hostenvProject.makeHostenv;
+      in mk config.hostenvProject.modules config.hostenvProject.environmentName;
 
     config.hostenvProject.outputs = {
       inherit (config.hostenvProject.eval.config) environments defaultEnvironment;
