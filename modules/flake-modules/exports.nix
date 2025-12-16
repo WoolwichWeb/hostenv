2214c63d5f7077047118290d453a3ca2a4ebd108
Liam McDermott <liam@woolwichweb.works>
2025-12-15 23:36:04 -0500

    modules: expose cliModule under flake attr

--- modules/flake-modules/exports.nix
+++ modules/flake-modules/exports.nix
@@ -2,6 +2,10 @@
 {
   config.flake = {
     modules = ../.;
+    # Primary exports for consumers: functions under flake.*
+    cliModule = ../cli-flake-module.nix;
+
+    # Back-compat: still expose under flake.lib for callers expecting the old path.
     lib = {
       cliModule = ../cli-flake-module.nix;
     };
