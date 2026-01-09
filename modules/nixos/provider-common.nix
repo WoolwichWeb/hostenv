f0fb0d978ca99c0688d9bdd9cdcb4fa0746a24f8
Liam McDermott <liam@woolwichweb.works>
2026-01-09 16:32:02 -0500

    Use mkDefault for provider defaults

--- modules/nixos/provider-common.nix
+++ modules/nixos/provider-common.nix
@@ -13,36 +13,36 @@
       };
 
       config = {
-        sops.defaultSopsFormat = "yaml";
-        sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+        sops.defaultSopsFormat = lib.mkDefault "yaml";
+        sops.age.sshKeyPaths = lib.mkDefault [ "/etc/ssh/ssh_host_ed25519_key" ];
 
         nix = {
           # Currently disabled `nix.settings.auto-optimise-store` as it seems to fail with remote builders
           # TODO re-enable when fixed https://github.com/NixOS/nix/issues/7273
-          settings.auto-optimise-store = false;
+          settings.auto-optimise-store = lib.mkDefault false;
 
           gc = {
-            automatic = true;
-            randomizedDelaySec = "14m";
-            options = "--delete-older-than 10d";
+            automatic = lib.mkDefault true;
+            randomizedDelaySec = lib.mkDefault "14m";
+            options = lib.mkDefault "--delete-older-than 10d";
           };
 
-          extraOptions = ''
+          extraOptions = lib.mkDefault ''
             experimental-features = nix-command flakes
             !include ${config.sops.secrets.access_tokens.path}
           '';
 
           # See: https://discourse.nixos.org/t/build-failed-due-to-lack-of-signature-by-trusted-key-in-distributed-build/44996/2
-          settings.trusted-users = [ "deploy" ];
+          settings.trusted-users = lib.mkDefault [ "deploy" ];
         };
 
         services.openssh = {
-          enable = true;
-          settings.PasswordAuthentication = false;
-          settings.KbdInteractiveAuthentication = false;
+          enable = lib.mkDefault true;
+          settings.PasswordAuthentication = lib.mkDefault false;
+          settings.KbdInteractiveAuthentication = lib.mkDefault false;
         };
 
-        security.sudo.extraRules = [{
+        security.sudo.extraRules = lib.mkDefault [{
           groups = [ "wheel" ];
           commands = [{
             command = "ALL";
@@ -50,25 +50,25 @@
           }];
         }];
 
-        users.groups.keys.name = "keys";
+        users.groups.keys.name = lib.mkDefault "keys";
 
         # Gitlab/Github access tokens for projects are kept in this key.
-        sops.secrets.access_tokens = {
+        sops.secrets.access_tokens = lib.mkDefault {
           mode = "0440";
           group = config.users.groups.keys.name;
         };
 
-        users.mutableUsers = false;
+        users.mutableUsers = lib.mkDefault false;
         users.users.deploy = {
-          isNormalUser = true;
-          extraGroups = [ "wheel" "keys" ];
+          isNormalUser = lib.mkDefault true;
+          extraGroups = lib.mkDefault [ "wheel" "keys" ];
           openssh.authorizedKeys.keys = lib.mkDefault deployPublicKeys;
         };
 
-        networking.firewall.enable = true;
-        networking.firewall.allowedTCPPorts = [ 80 443 22 ];
+        networking.firewall.enable = lib.mkDefault true;
+        networking.firewall.allowedTCPPorts = lib.mkDefault [ 80 443 22 ];
 
-        environment.sessionVariables = {
+        environment.sessionVariables = lib.mkDefault {
           XDG_CACHE_HOME = "$HOME/.cache";
           XDG_CONFIG_HOME = "$HOME/.config";
           XDG_DATA_HOME = "$HOME/.local/share";
