bf6dd6a5eef63fee640c7258250911e93603213f
Liam McDermott <liam@woolwichweb.works>
2026-02-16 22:17:46 -0500

    Configure provider-service via file-based runtime config.

--- modules/services/hostenv-provider-service/Main.hs
+++ modules/services/hostenv-provider-service/Main.hs
@@ -9,13 +9,25 @@ import Hostenv.Provider.Project (syncFlakeFromDb)
 import Hostenv.Provider.Repo (ensureGitConfig, ensureProviderRepo)
 import Hostenv.Provider.Server (runServer)
 import Hostenv.Provider.Service (CommandSpec (..))
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
 main = do
-    cfg <- loadConfig
+    args <- getArgs
+    configPath <- case args of
+      ["--config", path] -> pure path
+      _ -> dieWithUsage
+    cfg <- loadConfig configPath
     ensureProviderRepo cfg
     ensureGitConfig cfg
     ensureSchema cfg
     syncFlakeFromDb cfg
     runCommandOrDie cfg (CommandSpec "nix" ["flake", "update"] (appWorkDir cfg))
     runServer cfg
+
+dieWithUsage :: IO a
+dieWithUsage = do
+  hPutStrLn stderr "usage: hostenv-provider-service --config <path>"
+  exitFailure
