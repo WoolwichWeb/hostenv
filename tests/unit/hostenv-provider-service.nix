b9bf949398c5c5fdc26ac00ffe0ac34554aeb90d
Liam McDermott <liam@woolwichweb.works>
2026-02-23 11:55:28 -0500

    Fix provider-service checks to use cabal instead of runGhc.

--- tests/unit/hostenv-provider-service.nix
+++ tests/unit/hostenv-provider-service.nix
@@ -2,20 +2,9 @@
 let
   src = ../../modules/services/hostenv-provider-service;
   servicePkg = pkgs.haskellPackages.callCabal2nix "hostenv-provider-service" src { };
-  ghc = pkgs.haskell.packages.ghc912.ghcWithPackages (p: [
-    p.aeson
-    p.bytestring
-    p.crypton
-    p.memory
-    p.text
-  ]);
 
 in
 {
   hostenv-provider-service-build = servicePkg;
-  hostenv-provider-service-tests = pkgs.runCommand "hostenv-provider-service-tests" { } ''
-    set -euo pipefail
-    ${ghc}/bin/runghc -i${src} ${src}/Test.hs
-    echo ok > $out
-  '';
+  hostenv-provider-service-tests = pkgs.haskell.lib.doCheck servicePkg;
 }
