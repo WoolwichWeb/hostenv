9700c11fd770af560686e83a8b54c569cdfee0b9
Liam McDermott <liam@woolwichweb.works>
2026-04-27 10:55:09 -0400

    Remove workaround allowing Drupal projects to have insecure certs.

--- modules/features/drupal.nix
+++ modules/features/drupal.nix
@@ -61,13 +61,12 @@
                 method = "GET";
                 targetHostSource = "nodeConnectionHost";
                 followRedirects = true;
-              maxRedirects = 5;
-              timeoutSeconds = 15;
-              tlsMode = if canonicalVHostConfig.enableLetsEncrypt then "insecure" else "strict";
-            };
-            constraints = drupalVerificationConstraints;
-          }
-        ];
+                maxRedirects = 5;
+                timeoutSeconds = 15;
+              };
+              constraints = drupalVerificationConstraints;
+            }
+          ];
         };
       canonicalVHost = canonicalVHostFor env;
       canonicalVHostConfig = env.virtualHosts.${canonicalVHost};
